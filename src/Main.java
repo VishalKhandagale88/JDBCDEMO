8bd451974dc82e911f5310addcf3dac8ae1d74ac
vishal <vishal88kh@gmail.com>
2024-08-09 20:54:32 +0530

    added my-sql-connector and added logic to connect with my-sql  db

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+     static final String JDBC_URL = "jdbc:mysql://localhost:3306";
+     static final String USER = "root";
+     static final String PASSWORD = "root";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        try {
+            Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            System.out.println("Connected to database");
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException);
+            sqlException.printStackTrace();
+        }
+
     }
 }
