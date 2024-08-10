3d260e9f4fa9de4cef169ad3be137b858b3532b7
vishal <vishal88kh@gmail.com>
2024-08-10 09:12:08 +0530

    added statement and prepared statement

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import com.mysql.cj.xdevapi.PreparableStatement;
+
+import java.sql.*;
 
 public class Main {
 
-     static final String JDBC_URL = "jdbc:mysql://localhost:3306";
+     static final String JDBC_URL = "jdbc:mysql://localhost:3306/JDBCdemo";
      static final String USER = "root";
      static final String PASSWORD = "root";
 
@@ -13,6 +13,31 @@ public class Main {
         try {
             Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
             System.out.println("Connected to database");
+
+            Statement statement = connection.createStatement();
+
+//            String insertUSerQuery = "INSERT INTO user(name,email) VALUES ('Ayyan','Ayyan@gmail.com')";
+
+//            statement.executeUpdate(insertUSerQuery);
+
+            String insertQuery = "INSERT INTO  user(name,email) VALUES (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setString(1,"yoda");
+            preparedStatement.setString(2,"yoda@gmail.com");
+            preparedStatement.executeUpdate();
+
+            String selectAllUsers = "SELECT * FROM user";
+
+            PreparedStatement preparedStatementSelect = connection.prepareStatement(selectAllUsers);
+
+            ResultSet resultSet = preparedStatementSelect.executeQuery();
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                System.out.println("id : "+id +"name : "+name+" email : "+email);
+            }
+
         } catch (SQLException sqlException) {
             System.out.println(sqlException);
             sqlException.printStackTrace();
